9776bed4f698401f5541f3b9ece9b1aacf99d9eb
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-05-25 19:05:53 +0200

    snack 5-6

--- function.js
+++ function.js
@@ -6,11 +6,15 @@ function getInitials(name) {
   return `${letteraUno}.${letteraDue}.`;
 }
 
-// snack 2
+// snack 2 & 4
 function createSlug(string) {
   const lowerString = string.toLowerCase();
   const array = lowerString.split(" ");
-  if (array.length > 0) {
+  console.log(string.trim());
+
+  if (string.trim() === "") {
+    throw new Error("error title not valid");
+  } else if (array.length > 0) {
     return array.join("-");
   } else {
     return lowerString;
@@ -21,4 +25,14 @@ function createSlug(string) {
 function avarage(arrayNumber) {
   return arrayNumber.reduce((acc, num) => acc + num / arrayNumber.length, 0);
 }
-module.exports = { getInitials, createSlug, avarage };
+
+// snack 5
+function isPalindrome(string) {
+  const arrayString = string.split("");
+  const reverseString = arrayString.reverse().join("").toLowerCase();
+
+  return reverseString === string.toLowerCase();
+}
+
+// snack
+module.exports = { getInitials, createSlug, avarage, isPalindrome };
--- function.test.js
+++ function.test.js
@@ -1,6 +1,11 @@
 // snack 1
 
-const { getInitials, createSlug, avarage } = require("./function");
+const {
+  getInitials,
+  createSlug,
+  avarage,
+  isPalindrome,
+} = require("./function");
 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
   expect(getInitials("Gozzi Manuel")).toBe("G.M.");
@@ -21,7 +26,20 @@ test("La funzione average calcola la media aritmetica di un array di numeri.", (
 });
 
 // snack 4
+
 test("La funzione createSlug sostituisce gli spazi con -.", () => {
   const result = createSlug("test stringa join");
   expect(result).toBe("test-stringa-join");
+});
+
+// snack 5
+
+test("La funzione isPalindrome verifica se una stringa è un palindromo.", () => {
+  expect(isPalindrome("Anna")).toBe(true);
+  expect(isPalindrome("Paolo")).toBe(false);
+});
+
+// snack 6
+test("La funzione createSlug lancia un errore se il titolo è vuoto o non valido.", () => {
+  expect(() => createSlug("")).toThrow("error title not valid");
 });
