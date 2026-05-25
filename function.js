1085a28cfc1eea05c49e6992a09aa2face491066
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-05-25 18:30:22 +0200

    snack 2,3,4

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+// snack 1
 function getInitials(name) {
   const parole = name.split(" ");
   const letteraUno = parole[0].charAt(0).toUpperCase();
@@ -5,4 +6,19 @@ function getInitials(name) {
   return `${letteraUno}.${letteraDue}.`;
 }
 
-module.exports = { getInitials };
+// snack 2
+function createSlug(string) {
+  const lowerString = string.toLowerCase();
+  const array = lowerString.split(" ");
+  if (array.length > 0) {
+    return array.join("-");
+  } else {
+    return lowerString;
+  }
+}
+
+// snack 3
+function avarage(arrayNumber) {
+  return arrayNumber.reduce((acc, num) => acc + num / arrayNumber.length, 0);
+}
+module.exports = { getInitials, createSlug, avarage };
--- function.test.js
+++ function.test.js
@@ -1,6 +1,27 @@
-const { getInitials } = require("./function");
+// snack 1
+
+const { getInitials, createSlug, avarage } = require("./function");
 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
   expect(getInitials("Gozzi Manuel")).toBe("G.M.");
   expect(getInitials("gozzi manuel")).toBe("G.M.");
+});
+
+// Snack 2
+
+test("La funzione createSlug restituisce una stringa in lowercase.", () => {
+  expect(createSlug("StrIngA")).toBe("stringa");
+});
+
+// snack 3
+
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+  const result = avarage([1, 5, 3, 6, 10]);
+  expect(result).toBe(5);
+});
+
+// snack 4
+test("La funzione createSlug sostituisce gli spazi con -.", () => {
+  const result = createSlug("test stringa join");
+  expect(result).toBe("test-stringa-join");
 });
